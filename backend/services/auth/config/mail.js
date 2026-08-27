0a1e5c7039acb09d09d78d8899ebf68fbd999352
satyajit7679 <satyajitbarik987@gmail.com>
2026-08-27 12:23:24 +0530

    change auth

--- backend/services/auth/config/mail.js
+++ backend/services/auth/config/mail.js
@@ -2,13 +2,13 @@ import "dotenv/config";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD?.replace(/\s/g, ""),
-  },
+    host: process.env.EMAIL_HOST || "smtp.gmail.com",
+    port: Number(process.env.EMAIL_PORT || 587),
+    secure: process.env.EMAIL_SECURE === "true",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+    }
 });
 
 export const sendOtpEmail = async (email, otp) => {
